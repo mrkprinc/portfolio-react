ba640d565d230d0880cbe132d9be834b7dc276ae
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-22 15:42:09 -0400

    projectEntry styles

--- src/components/ProjectEntry.js
+++ src/components/ProjectEntry.js
@@ -10,7 +10,7 @@ const ProjectEntry = (props) => {
           <img src='./assets/images/FriendFinder-screenshot.PNG' alt='screenshot' />
         </div>
         <div className='text'>
-          <p>FriendFinder</p>
+          <h3>FriendFinder</h3>
           <p>A full-stack "find-your-best-match" app, using Express.js and Node.</p>
           <p><span role='img' aria-label='live'>🔴</span> <a href='https://fast-dusk-77293.herokuapp.com/' target='_blank' rel='noopener noreferrer'>Live Page</a></p>
           <p><span role='img' aria-label='github'>💻</span> <a href='https://github.com/mrkprinc/FriendFinder' target='_blank' rel='noopener noreferrer'>Github</a></p>
